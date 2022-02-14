7092d4f26fce88894b7dd59614674c26efebb672
unknown <mac991@gmail.com>
2022-02-14 16:39:22 -0500

    added stubs for userDAO, updated sql script to set size for unique ids(and their foreign keys) to 250

--- WeddingPlanner/src/main/java/com/revature/wedding_planner/daos/UserDAO.java
+++ WeddingPlanner/src/main/java/com/revature/wedding_planner/daos/UserDAO.java
@@ -1,5 +1,37 @@
 package com.revature.wedding_planner.daos;
 
-public class UserDAO {
+import java.util.List;
+
+public class UserDAO<User> implements CrudDAO<User>{
+
+	@Override
+	public User create(User newObj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(User updatedObj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
