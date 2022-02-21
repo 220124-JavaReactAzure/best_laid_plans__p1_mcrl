29759aa0c9baa532ccd68bb68709ec7dd6e15f3b
mike <mac991@gmail.com>
2022-02-21 14:31:54 -0500

    implemented MealTypesService based on working implementation of UserService(no need for authentication, validation needs expansion)

--- WeddingPlanner/src/main/java/com/revature/wedding_planner/services/MealTypesService.java
+++ WeddingPlanner/src/main/java/com/revature/wedding_planner/services/MealTypesService.java
@@ -1,11 +1,69 @@
 package com.revature.wedding_planner.services;
 
+import java.util.List;
+
 import com.revature.wedding_planner.daos.MealTypesDAO;
+import com.revature.wedding_planner.exceptions.InvalidRequestException;
+import com.revature.wedding_planner.exceptions.ResourcePersistenceException;
+import com.revature.wedding_planner.models.MealTypes;
+
 
 public class MealTypesService {
-
+	private final MealTypesDAO mealTypesDAO;
+	
 	public MealTypesService(MealTypesDAO mealTypesDAO) {
-		// TODO Auto-generated constructor stub
+		this.mealTypesDAO = new MealTypesDAO();
+	}
+	
+	public boolean addMealType(MealTypes mealType) {
+		if(!isValidMealType(mealType)) {
+			throw new InvalidRequestException("Invalid mealType data provided");
+		}
+		
+		MealTypes persistedMealType = mealTypesDAO.create(mealType);
+		
+		if (persistedMealType == null) {
+			throw new ResourcePersistenceException("The mealType could not be persisted");
+		}
+		
+		return true;
+	}
+	
+	public MealTypes updateMealType(MealTypes updatedMealType) {
+		if (!mealTypesDAO.update(updatedMealType)) {
+			throw new ResourcePersistenceException("Failure updating mealType.");
+		}
+		return updatedMealType;
+	}
+	public MealTypes getMealTypeByID(int mealTypeId) {
+
+		MealTypes foundMealType = mealTypesDAO.findById(mealTypeId);
+
+		if (foundMealType != null) {
+			return foundMealType;
+		} else {
+			return null;
+		}
 	}
 
+	public List<MealTypes> getAllMealTypes() {
+		List<MealTypes> allUsers = mealTypesDAO.findAll();
+		
+		if (allUsers == null) {
+			return null;
+		} else {
+			return allUsers;
+		}
+	}
+	
+	public void deleteUser(MealTypes mealType) {
+		mealTypesDAO.delete(mealType.getId());
+	}
+	private boolean isValidMealType(MealTypes mealType) {
+		// TODO expand validity checking
+		if(mealType != null) {
+			return true;
+		}
+		return false;
+	}
 }
